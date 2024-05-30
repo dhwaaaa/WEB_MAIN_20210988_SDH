632e8a2f072a440e08ac7ba517b6975d75de86b7
dhwa <dk9963@sungkyul.ac.kr>
2024-05-30 15:50:06 +0900

    5주차 LOGO_2추가 + 연습문제

--- JS/search.js
+++ JS/search.js
@@ -1,11 +1,19 @@
 document.getElementById("search_button_msg").addEventListener('click', search_message);
 
 
-function search_message(){alert("검색을 수행합니다!");}
+function search_message(){
+    const keywordInput = document.getElementById('typekeyword');
+
+    alert("검색을 수행합니다!");
+}
 
 function googleSearch() {
     const searchTerm = document.getElementById("search_input").value;
+    if(searchTerm === '' || searchTerm === '병'){
+        return false;
+    }
+    
     const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
     window.open(googleSearchUrl, "_blank");
     return false;
-    }
+}
