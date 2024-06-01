10b05dc60ef2f2e36ae8fd8f0fe77ca49b38ef74
dhwa <dk9963@sungkyul.ac.kr>
2024-06-01 22:00:33 +0900

    7주차 연습문제

--- JS/search.js
+++ JS/search.js
@@ -1,16 +1,15 @@
 const search_message = () => {
     alert("검색을 수행합니다!");
 }
-
 function googleSearch() {
     const searchTerm = document.getElementById("search_input").value;
-    if(searchTerm === '' || searchTerm === '병'){
+    if (searchTerm === '' || searchTerm === '병') {
         return false;
     }
-    
-    const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
+    const googleSearchUrl = `https://www.google.com/search?q=${
+        encodeURIComponent(searchTerm)
+    }`;
     window.open(googleSearchUrl, "_blank");
     return false;
 }
-
 document.getElementById("search_button_msg").addEventListener('click', search_message);
