168aa9f845b93ba887043fe7528c96f0444140b7
dhwa <dk9963@sungkyul.ac.kr>
2024-06-09 02:31:57 +0900

    14주차 완료

--- JS/login.js
+++ JS/login.js
@@ -105,90 +105,6 @@ const check_input = ()  => {
 //     return sanitizedInput;
 // };
  
-function setCookie(name, value, expiredays){
-    var date = new Date();
-    date.setDate(date.getDate() + expiredays);
-    document.cookie = escape(name) + "=" + escape(value) + ";expires=" + date.toUTCString() + "; path=/" + ";SameSite=None; Secure";
-}
-
-function getCookie(name){
-    var cookie = document.cookie;
-    console.log("쿠키를 요청합니다.");
-    if(cookie != ""){
-        var cookie_array = cookie.split(";");
-        for(var index in cookie_array){
-            var cookie_name = cookie_array[index].split("=");
-            
-            if(cookie_name[0] == "id"){
-                return cookie_name[1];
-            }
-        }
-    }
-    return;
-}
-
-function session_set() { //세션 저장
-    let session_id = document.querySelector("#typeEmailX"); // DOM 트리에서 ID 검색
-    let session_pass = document.querySelector("#typePasswordX"); // DOM 트리에서 pass 검색
-    if (sessionStorage) {
-        let en_text = encrypt_text(session_pass.value);
-        sessionStorage.setItem("Session_Storage_id", session_id.value);
-        sessionStorage.setItem("Session_Storage_pass", en_text);
-    } else {
-        alert("로컬 스토리지 지원 x");
-    }
-}
-
-function session_get() { //세션 읽기
-    if (sessionStorage) {
-        return sessionStorage.getItem("Session_Storage_pass");
-    } else {
-        alert("세션 스토리지 지원 x");
-    }
-}
-
-function session_check(){
-    if(sessionStorage.getItem("Session_Storage_id")){
-        alert("이미 로그인 되었습니다.");
-        location.href='../login/index_login.html'; // 로그인된 페이지로 이동
-    }
-}
-
-function encodeByAES256(key, data){
-    const cipher = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), {
-    iv: CryptoJS.enc.Utf8.parse(""),
-    padding: CryptoJS.pad.Pkcs7,
-    mode: CryptoJS.mode.CBC
-    });
-    return cipher.toString();
-}
-
-function decodeByAES256(key, data){
-    const cipher = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(key), {
-    iv: CryptoJS.enc.Utf8.parse(""),
-    padding: CryptoJS.pad.Pkcs7,
-    mode: CryptoJS.mode.CBC
-    });
-    return cipher.toString(CryptoJS.enc.Utf8);
-}
-      
-function encrypt_text(password){
-    const k = "key"; // 클라이언트 키
-    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
-    const b = password;
-    const eb = this.encodeByAES256(rk, b);
-    return eb;
-    console.log(eb);
-}
-
-function decrypt_text(){
-    const k = "key"; // 서버의 키
-    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
-    const eb = session_get();
-    const b = this.decodeByAES256(rk, eb);
-    console.log(b);
-}
-
 function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
     const emailInput = document.getElementById('typeEmailX');
     const idsave_check = document.getElementById('idSaveCheck');
@@ -200,7 +116,6 @@ function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
     }
     session_check();
 }
-init()
 
 function init_logined(){
     if(sessionStorage){
@@ -211,5 +126,16 @@ function init_logined(){
     }
 }
 
+function addJavascript(jsname) { // 자바스크립트 외부 연동
+    var th = document.getElementsByTagName('head')[0];
+    var s = document.createElement('script');
+    s.setAttribute('type','text/javascript');
+    s.setAttribute('src',jsname);
+    th.appendChild(s);
+}
+
+addJavascript('/js/security.js'); // 암복호화 함수
+addJavascript('/js/session.js'); // 세션 함수
+addJavascript('/js/cookie.js'); // 쿠키 함수
 
 document.getElementById("login_btn").addEventListener('click', check_input);
--- JS/logout.js
+++ JS/logout.js
@@ -8,11 +8,13 @@ function session_del(){
     if(sessionStorage){
         sessionStorage.removeItem("Session_Storage_id");
         sessionStorage.removeItem("Session_Storage_pass");
-        alert("로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.");
+        
+        document.cookie = "id=; " + "expires = Mon, 01, Jan 0000 00:00:00 UTC" + "; path=/";
+        alert("로그아웃 버튼 클릭 확인 : 세션 스토리지 및 쿠키를 삭제합니다.");
     }
     else{
         alert("세션 스토리지 지원 X");
     }
-}   
+}
 
 document.getElementById("logout_btn").addEventListener('click', check_input);
